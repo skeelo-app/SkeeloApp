c85023c1b0fc6511e9d9742f5ea7035c2df66d33
Quinhas <ln.santana08@gmail.com>
2019-11-21 12:07:38 -0300

    Commit 21/11/2019 - 12/07

--- src/app/pages/store-list/store-list.page.ts
+++ src/app/pages/store-list/store-list.page.ts
@@ -9,6 +9,8 @@ import { SkeeloApiService } from 'src/app/services/skeeloApi/skeelo-api.service'
 export class StoreListPage implements OnInit {
 
   public stores;
+  public completeList;
+  public search: string = '';
 
   constructor(
     private skeeloAPI: SkeeloApiService
@@ -20,8 +22,18 @@ export class StoreListPage implements OnInit {
   ionViewDidEnter() {
     this.skeeloAPI.getAllStores().subscribe((value) => {
       this.stores = value;
+      this.completeList = value;
     })
-    // console.log("TCL: StoreListPage -> ionViewWillEnter -> this.stores", this.stores)
+  }
+
+  filterList(list, filter) {
+    return list.filter(item => {
+      return item.store_displayname.toLowerCase().indexOf(filter.toLowerCase()) > -1;
+    })
+  }
+
+  filter(){
+    this.stores = this.filterList(this.completeList, this.search)
   }
 
 }
